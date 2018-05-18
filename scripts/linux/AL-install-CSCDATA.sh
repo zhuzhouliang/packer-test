5a6df1af4da3ff4b6ac3bf38b2872607d8653133
Zhu <zhou-liang.zhu@hpe.com>
2018-05-18 14:22:37 +0800

    Remove the error rpm

--- scripts/linux/AL-install-CSCDATA.sh
+++ scripts/linux/AL-install-CSCDATA.sh
@@ -2,13 +2,5 @@ echo \"downloading hardening software\"
 
 cd /tmp/soe_harden 
 # expand harden script 
-gunzip SOE-harden-3.1-5-linux-x86_64-client-SIP.sh.gz
-chmod 755 SOE-perl-5.22.1-0-linux-redhat-SIP.sh
-sh ./SOE-perl-5.22.1-0-linux-redhat-SIP.sh -i 
-if [[ ! $? -eq 0 ]]; then 
-  echo \"issue executing DXC hardening script: SOE-perl-5.22.1-0-linux-redhat-SIP.sh\" 
-  exit 1 
-fi 
-
 echo \"hardening successful\" 
 exit 0 \n"
